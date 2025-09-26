f37e1e391f49aa22cae27fdcf6e1d43b8839964a
abinandd <112941816+abinandd@users.noreply.github.com>
2025-09-26 11:05:04 +0530

    Basic implementation of binary search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,32 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 7, 8, 10, 16, 18, 40 };
+        int target = 40;
+        int result = binarySearch(arr, target);
+        if (result >= 0) {
+            System.out.println("Element found at " + result + " " + arr[result]);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
